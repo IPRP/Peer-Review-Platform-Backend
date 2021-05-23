dcbdba927cece07d90a63e1565d539bc0a376dd1
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2021-05-23 21:00:00 +0200

    Ported remaining data models Data models are now implemented as tables for MySQL.

---
+++ migrations/2021-05-23-182848_create_submissions/up.sql
@@ -0,0 +1,69 @@
+-- Your SQL goes here
+CREATE TABLE attachments
+(
+    id    SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    owner BIGINT UNSIGNED,
+    FOREIGN KEY (owner) REFERENCES users (id) ON DELETE SET NULL
+);
+
+CREATE TABLE submissions
+(
+    id          SERIAL PRIMARY KEY,
+    title       VARCHAR(255)    NOT NULL,
+    comment     TEXT            NOT NULL,
+    student     BIGINT UNSIGNED,
+    workshop    BIGINT UNSIGNED NOT NULL,
+    date        DATE            NOT NULL,
+    locked      BOOL            NOT NULL,
+    reviewsdone BOOL            NOT NULL,
+    error       BOOl            NOT NULL,
+    meanpoints  DOUBLE,
+    maxpoint    DOUBLE,
+    FOREIGN KEY (student) REFERENCES users (id) ON DELETE SET NULL,
+    FOREIGN KEY (workshop) REFERENCES workshops (id) ON DELETE CASCADE
+);
+
+CREATE TABLE submissioncriteria
+(
+    submission BIGINT UNSIGNED NOT NULL,
+    criterion  BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (submission, criterion),
+    FOREIGN KEY (submission) REFERENCES submissions (id) ON DELETE CASCADE,
+    FOREIGN KEY (criterion) REFERENCES criterion (id) ON DELETE CASCADE
+);
+
+CREATE TABLE submissionattachments
+(
+    submission BIGINT UNSIGNED NOT NULL,
+    attachment BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (submission, attachment),
+    FOREIGN KEY (submission) REFERENCES submissions (id) ON DELETE CASCADE,
+    FOREIGN KEY (attachment) REFERENCES attachments (id) ON DELETE CASCADE
+);
+
+CREATE TABLE reviews
+(
+    id         SERIAL PRIMARY KEY,
+    feedback   TEXT            NOT NULL,
+    reviewer   BIGINT UNSIGNED,
+    submission BIGINT UNSIGNED NOT NULL,
+    workshop   BIGINT UNSIGNED NOT NULL,
+    deadline   DATE            NOT NULL,
+    done       BOOL            NOT NULL,
+    locked     BOOL            NOT NULL,
+    error      BOOl            NOT NULL,
+    FOREIGN KEY (reviewer) REFERENCES users (id) ON DELETE SET NULL,
+    FOREIGN KEY (submission) REFERENCES submissions (id) ON DELETE CASCADE,
+    FOREIGN KEY (workshop) REFERENCES workshops (id) ON DELETE CASCADE
+);
+
+CREATE TABLE reviewpoints
+(
+    review    BIGINT UNSIGNED NOT NULL,
+    criterion BIGINT UNSIGNED NOT NULL,
+    points    DOUBLE,
+    PRIMARY KEY (review, criterion),
+    FOREIGN KEY (review) REFERENCES submissions (id) ON DELETE CASCADE,
+    FOREIGN KEY (criterion) REFERENCES criterion (id) ON DELETE CASCADE
+);
--- src/schema.rs
+++ src/schema.rs
@@ -2,6 +2,17 @@ table! {
     use diesel::sql_types::*;
     use crate::models::*;
 
+    attachments (id) {
+        id -> Unsigned<Bigint>,
+        title -> Varchar,
+        owner -> Nullable<Unsigned<Bigint>>,
+    }
+}
+
+table! {
+    use diesel::sql_types::*;
+    use crate::models::*;
+
     criteria (workshop, criterion) {
         workshop -> Unsigned<Bigint>,
         criterion -> Unsigned<Bigint>,
@@ -18,6 +29,73 @@ table! {
         content -> Text,
         weight -> Nullable<Double>,
         kind -> KindMapping,
+    }
+}
+
+table! {
+    use diesel::sql_types::*;
+    use crate::models::*;
+
+    reviewpoints (review, criterion) {
+        review -> Unsigned<Bigint>,
+        criterion -> Unsigned<Bigint>,
+        points -> Nullable<Double>,
+    }
+}
+
+table! {
+    use diesel::sql_types::*;
+    use crate::models::*;
+
+    reviews (id) {
+        id -> Unsigned<Bigint>,
+        feedback -> Text,
+        reviewer -> Nullable<Unsigned<Bigint>>,
+        submission -> Unsigned<Bigint>,
+        workshop -> Unsigned<Bigint>,
+        deadline -> Date,
+        done -> Bool,
+        locked -> Bool,
+        error -> Bool,
+    }
+}
+
+table! {
+    use diesel::sql_types::*;
+    use crate::models::*;
+
+    submissionattachments (submission, attachment) {
+        submission -> Unsigned<Bigint>,
+        attachment -> Unsigned<Bigint>,
+    }
+}
+
+table! {
+    use diesel::sql_types::*;
+    use crate::models::*;
+
+    submissioncriteria (submission, criterion) {
+        submission -> Unsigned<Bigint>,
+        criterion -> Unsigned<Bigint>,
+    }
+}
+
+table! {
+    use diesel::sql_types::*;
+    use crate::models::*;
+
+    submissions (id) {
+        id -> Unsigned<Bigint>,
+        title -> Varchar,
+        comment -> Text,
+        student -> Nullable<Unsigned<Bigint>>,
+        workshop -> Unsigned<Bigint>,
+        date -> Date,
+        locked -> Bool,
+        reviewsdone -> Bool,
+        error -> Bool,
+        meanpoints -> Nullable<Double>,
+        maxpoint -> Nullable<Double>,
     }
 }
 
@@ -60,9 +138,33 @@ table! {
     }
 }
 
+joinable!(attachments -> users (owner));
 joinable!(criteria -> criterion (criterion));
 joinable!(criteria -> workshops (workshop));
+joinable!(reviewpoints -> criterion (criterion));
+joinable!(reviewpoints -> submissions (review));
+joinable!(reviews -> submissions (submission));
+joinable!(reviews -> users (reviewer));
+joinable!(reviews -> workshops (workshop));
+joinable!(submissionattachments -> attachments (attachment));
+joinable!(submissionattachments -> submissions (submission));
+joinable!(submissioncriteria -> criterion (criterion));
+joinable!(submissioncriteria -> submissions (submission));
+joinable!(submissions -> users (student));
+joinable!(submissions -> workshops (workshop));
 joinable!(workshoplist -> users (user));
 joinable!(workshoplist -> workshops (workshop));
 
-allow_tables_to_appear_in_same_query!(criteria, criterion, users, workshoplist, workshops,);
+allow_tables_to_appear_in_same_query!(
+    attachments,
+    criteria,
+    criterion,
+    reviewpoints,
+    reviews,
+    submissionattachments,
+    submissioncriteria,
+    submissions,
+    users,
+    workshoplist,
+    workshops,
+);
